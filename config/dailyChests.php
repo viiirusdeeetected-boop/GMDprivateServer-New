56b4e9a94c22e6864d9dc66bfc17b1b579bb5ce2
viiirusdeeetected-boop <viiirusdeeetected@gmail.com>
2025-10-19 04:02:04 -0700

    Update dailyChests.php

--- config/dailyChests.php
+++ config/dailyChests.php
@@ -7,22 +7,22 @@
 	REWARDS
 */
 //SMALL CHEST
-$chest1minOrbs = 200;
-$chest1maxOrbs = 400;
-$chest1minDiamonds = 2;
-$chest1maxDiamonds = 10;
+$chest1minOrbs = 0;
+$chest1maxOrbs = 0;
+$chest1minDiamonds = 0;
+$chest1maxDiamonds = 0;
 $chest1items = [1, 2, 3, 4, 5, 6, 10, 11, 12, 13, 14];
-$chest1minKeys = 1;
+$chest1minKeys = 6;
 $chest1maxKeys = 6;
 //BIG CHEST
-$chest2minOrbs = 2000;
-$chest2maxOrbs = 4000;
-$chest2minDiamonds = 20;
-$chest2maxDiamonds = 100;
+$chest2minOrbs = 200000;
+$chest2maxOrbs = 3600000;
+$chest2minDiamonds = 2000;
+$chest2maxDiamonds = 19000;
 $chest2items = [1, 2, 3, 4, 5, 6, 10, 11, 12, 13, 14];
-$chest2minKeys = 1;
+$chest2minKeys = 6;
 $chest2maxKeys = 6;
 //REWARD TIMES (in seconds)
-$chest1wait = 3600;
-$chest2wait = 14400;
+$chest1wait = 0;
+$chest2wait = 0;
 ?>
